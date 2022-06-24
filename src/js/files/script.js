9413527ad7f033e6121da284f346da8f581ac451
Vadim Lyashenko <terateratera23@gmail.com>
2022-06-24 09:20:09 +0300

    feat(components): alert

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,11 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+let page = document.querySelector("main")
+
+function showAlert() {
+  if (page.classList.contains("show-alert")) return
+  setTimeout(() => page.classList.remove("show-alert"), 3600)
+  page.classList.add("show-alert")
+}
