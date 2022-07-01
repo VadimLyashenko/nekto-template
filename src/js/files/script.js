1554fa5d4c321cce7ca3aa7c64f4fed53f9ddf2a
Vadim Lyashenko <terateratera23@gmail.com>
2022-07-01 17:41:50 +0300

    feat(gallery): new images

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -22,6 +22,44 @@ function showAlert() {
 
 let galleries = document.querySelectorAll('[data-gallery]')
 
+let items_1 = [
+  {
+    src: '../img/products/chairs/chair_1/gallery_1.png',
+    thumb: '../img/products/chairs/chair_1/thumb_1.png',
+  },
+  {
+    src: '../img/products/chairs/chair_1/gallery_2.png',
+    thumb: '../img/products/chairs/chair_1/thumb_2.png',
+  },
+  {
+    src: '../img/products/chairs/chair_1/gallery_3.png',
+    thumb: '../img/products/chairs/chair_1/thumb_3.png',
+  },
+  {
+    src: '../img/products/chairs/chair_1/gallery_4.png',
+    thumb: '../img/products/chairs/chair_1/thumb_4.png',
+  },
+];
+
+let items_2 = [
+  {
+    src: '../img/products/chairs/chair_2/gallery_1.png',
+    thumb: '../img/products/chairs/chair_2/thumb_1.png',
+  },
+  {
+    src: '../img/products/chairs/chair_2/gallery_2.png',
+    thumb: '../img/products/chairs/chair_2/thumb_2.png',
+  },
+  {
+    src: '../img/products/chairs/chair_2/gallery_3.png',
+    thumb: '../img/products/chairs/chair_2/thumb_3.png',
+  },
+  {
+    src: '../img/products/chairs/chair_2/gallery_4.png',
+    thumb: '../img/products/chairs/chair_2/thumb_4.png',
+  },
+];
+
 galleries.forEach(gallery => {
 
   flsModules.gallery[0].galleryClass.getMediaContainerPosition = function () {
@@ -36,25 +74,14 @@ galleries.forEach(gallery => {
   }
 
   gallery.addEventListener('click', () => {
-    const items = [
-      {
-        src: '../img/products/chairs/chair_1/gallery_1.png',
-        thumb: '../img/products/chairs/chair_1/chair_1.png',
-      },
-      {
-        src: '../img/products/chairs/chair_1/gallery_2.png',
-        thumb: '../img/products/chairs/chair_1/chair_1.png',
-      },
-      {
-        src: '../img/products/chairs/chair_1/gallery_3.png',
-        thumb: '../img/products/chairs/chair_1/chair_1.png',
-      },
-      {
-        src: '../img/products/chairs/chair_1/gallery_4.png',
-        thumb: '../img/products/chairs/chair_1/chair_1.png',
-      },
-    ];
-    flsModules.gallery[0].galleryClass.refresh(items)
+    let id = +gallery.getAttribute("data-id")
+
+    if(id === 1) {
+      flsModules.gallery[0].galleryClass.refresh(items_1)
+    } else {
+      flsModules.gallery[0].galleryClass.refresh(items_2)
+    }
+    
     flsModules.gallery[0].galleryClass.openGallery()
   })
 })
